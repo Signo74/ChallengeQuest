95c47e16105cfa4ff5135ca055d1ac2adb62c5f0
Victor Marinov <victor.v.marinov@gmail.com>
2014-11-11 12:49:57 +0200

    Reverting QuestDBHelper

--- app/src/main/java/com/swinginpenguin/vmarinov/challengequest/db/dbhelper/QuestsDBHelper.java
+++ app/src/main/java/com/swinginpenguin/vmarinov/challengequest/db/dbhelper/QuestsDBHelper.java
@@ -8,6 +8,48 @@ import android.database.sqlite.SQLiteOpenHelper;
  * Created by victorm on 10/23/2014..
  */
 public class QuestsDBHelper extends SQLiteOpenHelper {
+    public static final String TABLE_NAME = "quests";
+    public static final String DATABASE_NAME = "main.db";
+    private static int DATABASE_VERSION = 1;
+    public static final String ID_COLUMN = "_id";
+    public static final String TYPE_COLUMN = "type";
+    public static final String TITLE_COLUMN = "title";
+    public static final String DESCRIPTION_COLUMN = "description";
+    public static final String CHAPTERS = "chapters";
+    public static final String EXP_REWARD = "experienceGranted";
+    public static final String RANK = "rank";
+    public static final String MAX_RANK = "maxrank";
+    public static final String COMPLETION = "percentagecompleted";
 
+    //TODO: Modify initial creation string to insert all necessary fields.
+    private static final String DATABASE_CREATE = "create table " + TABLE_NAME + "("
+            + ID_COLUMN + " integer primary key autoincrement, "
+            + TITLE_COLUMN + " text not null, "
+            + TYPE_COLUMN + " integer, "
+            + DESCRIPTION_COLUMN + " text, "
+            + CHAPTERS + " text, "
+            + EXP_REWARD + " text, "
+            + RANK + " text, "
+            + MAX_RANK + " text, "
+            + COMPLETION + " text, "
+            + ");";
 
+    public QuestsDBHelper(Context context) {
+        super(context, TABLE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(DATABASE_CREATE);
+    }
+
+    private void createTable(SQLiteDatabase sqLiteDatabase) {
+        //TODO: initialize the DB if it is missing.
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldDB, int newDB) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME + ";");
+        onCreate(sqLiteDatabase);
+    }
 }
